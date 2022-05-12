f0087d6d19e15ea24e01b635127d9a22937e37bb
linzhibo <zhibo98@hotmail.com>
2022-05-12 08:24:36 +0800

    Changed naming

--- PathTracking/stanley.js
+++ PathTracking/stanley.js
@@ -3,12 +3,12 @@ class StanleyController{
     constructor(){
         this.k = 0.5;  // control gain
         this.Kp = 1.0;  // speed proportional gain
-        this.last_target_index = -1;
+        this.current_idx = -1;
         this.old_nearest_point_index = -1;
     }
 
     reset() {
-        this.last_target_index = -1;
+        this.current_idx = -1;
     }
 
     calc_target_index(car, path){
@@ -31,21 +31,21 @@ class StanleyController{
     }
 
     compute_cmd(car, path){
-        let [current_target_index, error_front_axle] = this.calc_target_index(car, path);
-        if (this.last_target_index >= current_target_index){
-            current_target_index = this.last_target_index;
+        let [target_index, error_front_axle] = this.calc_target_index(car, path);
+        if (this.current_idx >= target_index){
+            target_index = this.current_idx;
         }
-        let theta_e = normalize_angle(path.yaw[current_target_index] - car.yaw);
+        let theta_e = normalize_angle(path.yaw[target_index] - car.yaw);
         let theta_d = Math.atan2(this.k * error_front_axle, car.speed);
         let delta = theta_e + theta_d;
-        let direction = path.directions[current_target_index];
+        let direction = path.directions[target_index];
         let pedal = direction * this.proportional_control(car.max_speed, car.speed);
         let arrived = false;
         let dist_to_goal = car.calc_distance_to_point(new Vector2D(path.x[path.x.length - 1], path.y[path.y.length - 1]));
         if (dist_to_goal < 10) {
             arrived = true;
         }
-        this.last_target_index = current_target_index;
+        this.current_idx = target_index;
         return [pedal, delta, arrived];
     }
 }
